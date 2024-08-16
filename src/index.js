a90251136498589e331a7590495ccb3caf15bb48
Dmytro Iovenko <iovenko@gmail.com>
2024-08-16 11:12:57 -0400

    adding Registration Form - Username Validation

--- src/index.js
+++ src/index.js
@@ -9,7 +9,38 @@ const handleLoad = () => {
   function validateRegistration(event) {
     // prevent default submit logic
     event.preventDefault();
-    displayError("test error");
+    try {
+      usernameValidation(registrationForm.elements.username.value);
+    } catch (err) {
+      displayError(err.message);
+      console.log(err);
+    }
+  }
+
+  // Registration Form - Username Validation:
+
+  function usernameValidation(username) {
+    console.log(username);
+    //The username cannot be blank.
+    if (username === "") {
+      throw new Error("The username cannot be blank.");
+    }
+    //The username must be at least four characters long.
+    if (username.length < 4) {
+      throw new Error("The username must be at least four characters long.");
+    }
+    //The username must contain at least two unique characters.
+    if (username.match(/^(.)\1+$/)) {
+      throw new Error(
+        "The username must contain at least two unique characters."
+      );
+    }
+    //The username cannot contain any special characters or whitespace.
+    if (username.match(/\s|\W/)) {
+      throw new Error(
+        "The username cannot contain any special characters or whitespace."
+      );
+    }
   }
 
   function displayError(error) {
